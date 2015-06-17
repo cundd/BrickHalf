0de1ad0753f613b42d487d177ae0fe8e0508b70e
Daniel Corn <d@cundd.net>
2015-06-17 19:48:13 +0200

    Formatted source code

--- main.cpp
+++ main.cpp
@@ -1,29 +1,29 @@
 #include <iostream>
 #include "MapParser.hpp"
 
-int main(int argc, char ** argv)
+int main(int argc, char** argv)
 {
-	std::string inputString;
-	char rowDelimiter = ';';
-	char pairSeperator = '=';
-	
-	if (argc < 2) {
-		std::getline(std::cin, inputString);
-	} else {
-		inputString = std::string(argv[1]);
-	}
+    std::string inputString;
+    char rowDelimiter = ';';
+    char pairSeperator = '=';
 
-	if (inputString.empty()) {
-		std::cerr << "Missing input string" << std::endl;
-		return 1;
-	}
-	MapParser parser(rowDelimiter, pairSeperator);
+    if (argc < 2) {
+        std::getline(std::cin, inputString);
+    }
+    else {
+        inputString = std::string(argv[1]);
+    }
 
-	std::map<std::string, std::string> parsedMap = parser.parse(inputString);
-	for (std::pair<std::string, std::string> pair : parsedMap) {
-		std::cout << "'" << pair.first << "' '" << pair.second << "'" << std::endl;
-	}
-	std::cout << std::endl;
-	return 0;
+    if (inputString.empty()) {
+        std::cerr << "Missing input string" << std::endl;
+        return 1;
+    }
+    MapParser parser(rowDelimiter, pairSeperator);
+
+    std::map<std::string, std::string> parsedMap = parser.parse(inputString);
+    for (std::pair<std::string, std::string> pair : parsedMap) {
+        std::cout << "'" << pair.first << "' '" << pair.second << "'" << std::endl;
+    }
+    std::cout << std::endl;
+    return 0;
 }
-
